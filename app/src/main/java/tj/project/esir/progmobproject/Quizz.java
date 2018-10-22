5adb383531f30d58ab9b21308d73828120515f94
AirJulien <julien.gueperoux@gmail.com>
2018-10-22 17:29:28 +0200

    Remise en place du retour au menu si appuie sur le bouton retour

--- app/src/main/java/tj/project/esir/progmobproject/Quizz.java
+++ app/src/main/java/tj/project/esir/progmobproject/Quizz.java
@@ -1,18 +1,13 @@
 package tj.project.esir.progmobproject;
 import org.json.*;
 
-import android.content.res.AssetManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Random;
 
@@ -31,12 +26,12 @@ public class Quizz extends AppCompatActivity {
     TextView reponseText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizz);
-
+        
         try {
-  gbvf
-            h// récupération du contenu du fichier questions.json
+            // récupération du contenu du fichier questions.json
             InputStream is = getAssets().open("questions.json");
             int size = is.available();
             byte[] buffer = new byte[size];
@@ -121,4 +116,28 @@ public class Quizz extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onBackPressed() {
+        Intent home = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(home);
+        finish();
+    }
+
+
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
 }
