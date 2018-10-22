eac4355b45a183e612d0ba387ab755becd2136a4
Yztoc <stephant.thomas.ae@gmail.com>
2018-10-22 10:48:23 +0200

    p

--- app/src/main/java/tj/project/esir/progmobproject/Quizz.java
+++ app/src/main/java/tj/project/esir/progmobproject/Quizz.java
@@ -16,7 +16,7 @@ public class Quizz extends AppCompatActivity {
         setContentView(R.layout.activity_quizz);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        hideSystemUI();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
