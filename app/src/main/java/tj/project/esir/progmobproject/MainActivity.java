cdbb3198597c2c6460e2c046c2be6a9f3cfcfc5e
AirJulien <julien.gueperoux@gmail.com>
2018-10-26 20:29:02 +0200

    suppression de la barre en haut de l'écran !

--- app/src/main/java/tj/project/esir/progmobproject/MainActivity.java
+++ app/src/main/java/tj/project/esir/progmobproject/MainActivity.java
@@ -15,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        hideSystemUI();
         setContentView(R.layout.activity_main);
 
         btnSinglePlayer = findViewById(R.id.btn_singleplayer);
@@ -39,23 +38,5 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println("button 3");
             }
         });
-    }
-
-
-    private void hideSystemUI() {
-        // Enables regular immersive mode.
-        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE
-                        // Set the content to appear under the system bars so that the
-                        // content doesn't resize when the system bars hide and show.
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        // Hide the nav bar and status bar
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
--- app/src/main/java/tj/project/esir/progmobproject/Quizz.java
+++ app/src/main/java/tj/project/esir/progmobproject/Quizz.java
@@ -29,7 +29,7 @@ public class Quizz extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizz);
-        
+
         try {
             // récupération du contenu du fichier questions.json
             InputStream is = getAssets().open("questions.json");
@@ -121,23 +121,5 @@ public class Quizz extends AppCompatActivity {
         Intent home = new Intent(getApplicationContext(),MainActivity.class);
         startActivity(home);
         finish();
-    }
-
-
-    private void hideSystemUI() {
-        // Enables regular immersive mode.
-        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE
-                        // Set the content to appear under the system bars so that the
-                        // content doesn't resize when the system bars hide and show.
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        // Hide the nav bar and status bar
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
