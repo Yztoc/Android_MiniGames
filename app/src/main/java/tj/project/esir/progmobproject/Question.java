cf22f1a5ea807dd492bac106aa8b65ab1f06180a
AirJulien <julien.gueperoux@gmail.com>
2018-10-31 18:51:18 +0100

    Ajout de la classe Question.java, du manager et du fichier de création de table

---
+++ app/src/main/java/tj/project/esir/progmobproject/Question.java
@@ -0,0 +1,60 @@
+package tj.project.esir.progmobproject;
+
+import android.util.Pair;
+
+public class Question {
+
+    private int id;
+    private String title;
+    private Pair<String,Integer> response1;
+    private Pair<String,Integer> response2;
+    private Pair<String,Integer> response3;
+
+    public Question(){
+        this.id = -1;
+        this.title = "";
+        this.response1 = new Pair<>("",0);
+        this.response2 = new Pair<>("",0);
+        this.response3 = new Pair<>("",0);
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Pair<String, Integer> getResponse1() {
+        return response1;
+    }
+
+    public void setResponse1(Pair<String, Integer> response1) {
+        this.response1 = response1;
+    }
+
+    public Pair<String, Integer> getResponse2() {
+        return response2;
+    }
+
+    public void setResponse2(Pair<String, Integer> response2) {
+        this.response2 = response2;
+    }
+
+    public Pair<String, Integer> getResponse3() {
+        return response3;
+    }
+
+    public void setResponse3(Pair<String, Integer> response3) {
+        this.response3 = response3;
+    }
+}
---
+++ app/src/main/java/tj/project/esir/progmobproject/QuestionManager.java
@@ -0,0 +1,124 @@
+package tj.project.esir.progmobproject;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Pair;
+
+public class QuestionManager {
+
+    private static final String TABLE_NAME = "question";
+    public static final String ID_QUESTION="id_question";
+    public static final String TITLE_QUESTION="title_question";
+    public static final String TEXT_RESPONSE1="text_reponse1";
+    public static final String TEXT_RESPONSE2="text_reponse2";
+    public static final String TEXT_RESPONSE3="text_reponse3";
+    public static final String VALUE_RESPONSE1="value_reponse1";
+    public static final String VALUE_RESPONSE2="value_reponse2";
+    public static final String VALUE_RESPONSE3="value_reponse3";
+
+    public static final String CREATE_TABLE_QUESTION = "CREATE TABLE "+TABLE_NAME+
+            " (" +
+            " "+ID_QUESTION+" INTEGER primary key,"+
+            " "+TITLE_QUESTION+" TEXT" +
+            " "+TEXT_RESPONSE1+" TEXT" +
+            " "+TEXT_RESPONSE2+" TEXT" +
+            " "+TEXT_RESPONSE3+" TEXT" +
+            " "+VALUE_RESPONSE1+" BOOLEAN" +
+            " "+VALUE_RESPONSE2+" BOOLEAN" +
+            " "+VALUE_RESPONSE3+" BOOLEAN" +
+            ");";
+
+    private MySQLite maBaseSQLite; // notre gestionnaire du fichier SQLite
+    private SQLiteDatabase db;
+
+    public QuestionManager(Context context) {
+        maBaseSQLite = MySQLite.getInstance(context);
+    }
+
+    public void open() {
+        //on ouvre la table en lecture/écriture
+        db = maBaseSQLite.getWritableDatabase();
+    }
+
+    public void close() {
+        //on ferme l'accès à la BDD
+        db.close();
+    }
+
+    public long addQuestion(Question question) {
+        // Ajout d'un enregistrement dans la table
+        ContentValues values = new ContentValues();
+        values.put(TITLE_QUESTION, question.getTitle());
+        values.put(TEXT_RESPONSE1, question.getResponse1().first);
+        values.put(TEXT_RESPONSE2, question.getResponse2().first);
+        values.put(TEXT_RESPONSE3, question.getResponse3().first);
+        values.put(VALUE_RESPONSE1, question.getResponse1().second);
+        values.put(VALUE_RESPONSE2, question.getResponse2().second);
+        values.put(VALUE_RESPONSE3, question.getResponse3().second);
+
+        // insert() retourne l'id du nouvel enregistrement inséré, ou -1 en cas d'erreur
+        return db.insert(
+                TABLE_NAME,null,values);
+    }
+
+
+    public int updateQuestion(Question question) {
+        // modification d'un enregistrement
+        // valeur de retour : (int) nombre de lignes affectées par la requête
+
+        ContentValues values = new ContentValues();
+        values.put(TITLE_QUESTION, question.getTitle());
+        values.put(TEXT_RESPONSE1, question.getResponse1().first);
+        values.put(TEXT_RESPONSE2, question.getResponse2().first);
+        values.put(TEXT_RESPONSE3, question.getResponse3().first);
+        values.put(VALUE_RESPONSE1, question.getResponse1().second);
+        values.put(VALUE_RESPONSE2, question.getResponse2().second);
+        values.put(VALUE_RESPONSE3, question.getResponse3().second);
+
+        String where = ID_QUESTION+" = ?";
+        String[] whereArgs = {question.getId()+""};
+
+        return db.update(TABLE_NAME, values, where, whereArgs);
+    }
+
+    public int deleteQuestion(Question question) {
+        // suppression d'un enregistrement
+        // valeur de retour : (int) nombre de lignes affectées par la clause WHERE, 0 sinon
+
+        String where = ID_QUESTION+" = ?";
+        String[] whereArgs = {question.getId()+""};
+
+        return db.delete(TABLE_NAME, where, whereArgs);
+    }
+
+    public Question getQuestion(int id) {
+        // Retourne l'animal dont l'id est passé en paramètre
+
+        Question question=new Question();
+
+        Cursor c = db.rawQuery(
+                "SELECT * FROM "+TABLE_NAME+" WHERE "+
+                        ID_QUESTION+"="+id, null);
+
+        if (c.moveToFirst()) {
+            question.setId(c.
+                    getInt(c.getColumnIndex(ID_QUESTION)));
+            question.setTitle(c.
+                    getString(c.getColumnIndex(TITLE_QUESTION)));
+            question.setResponse1(new Pair<>(c.getString(c.getColumnIndex(TEXT_RESPONSE1)),c.getInt(c.getColumnIndex(VALUE_RESPONSE1))));
+            question.setResponse2(new Pair<>(c.getString(c.getColumnIndex(TEXT_RESPONSE2)),c.getInt(c.getColumnIndex(VALUE_RESPONSE2))));
+            question.setResponse3(new Pair<>(c.getString(c.getColumnIndex(TEXT_RESPONSE3)),c.getInt(c.getColumnIndex(VALUE_RESPONSE3))));
+
+            c.close();
+        }
+        return question;
+    }
+
+    public Cursor getQuestions() {
+        // sélection de tous les enregistrements de la table
+        return db.rawQuery("SELECT * FROM "+TABLE_NAME,
+                null);
+    }
+}
