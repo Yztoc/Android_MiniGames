4d783d27c8728c30c7797758e5e43dac42bcefab
AirJulien <julien.gueperoux@gmail.com>
2018-11-16 22:09:24 +0100

    suppression de la méthode hideSystemUI de la call Balls.java qui était inutile

--- app/src/main/java/tj/project/esir/progmobproject/ball_games/Balls.java
+++ app/src/main/java/tj/project/esir/progmobproject/ball_games/Balls.java
@@ -89,7 +89,6 @@ public class Balls extends AppCompatActivity {
         goalY = 200;
 
         generateMap();
-        hideSystemUI();
 
         // Initialize gameView and set it as the view
         gameView = new GameView(this);
@@ -485,35 +484,11 @@ public class Balls extends AppCompatActivity {
             return true;
         }
     };
-
-
-
-
-
-
-
+    
     public void onBackPressed() {
         Intent home = new Intent(getApplicationContext(),MainActivity.class);
         startActivity(home);
         finish();
     }
-
-    private void hideSystemUI() {
-        // Enables regular immersive mode.
-        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE
-                        // Set the content to appear under the system bars so that the
-                        // content doesn't resize when the system bars hide and show.
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        // Hide the nav bar and status bar
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
-    }
-
 
 }
