4a916134d15a4360abd6a824c0008bf8ed26a804
Yztoc <stephant.thomas.ae@gmail.com>
2018-11-28 20:56:01 +0100

    create ScoreManager

---
+++ app/src/main/java/tj/project/esir/progmobproject/models/Score.java
@@ -0,0 +1,45 @@
+package tj.project.esir.progmobproject.models;
+
+public class Score {
+
+    private int id;
+    private String name_game;
+    private int score;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName_game() {
+        return name_game;
+    }
+
+    public void setName_game(String name_game) {
+        this.name_game = name_game;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Score(int id, String name_game, int score){
+        this.id=id;
+        this.name_game = name_game;
+        this.score = score;
+    }
+
+    public Score(){
+
+    }
+
+
+
+}
