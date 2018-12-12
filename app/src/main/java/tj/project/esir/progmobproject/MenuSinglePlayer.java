51d492b1665045248b8fd5ff07de0d7ca50e9982
Yztoc <stephant.thomas.ae@gmail.com>
2018-12-13 00:43:50 +0100

    tutorial activity + layout

---
+++ app/src/main/java/tj/project/esir/progmobproject/MenuSinglePlayer.java
@@ -0,0 +1,51 @@
+package tj.project.esir.progmobproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import tj.project.esir.progmobproject.ball_games.Balls;
+import tj.project.esir.progmobproject.ball_games.MenuParam;
+
+public class MenuSinglePlayer extends AppCompatActivity {
+
+    private Button btnTuto;
+    private Button btnGame;
+    private static String KEY_VALUE_LEVEL = "level";
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_single_player);
+
+        btnTuto = findViewById(R.id.btn_tuto);
+        btnGame = findViewById(R.id.btn_game);
+
+        btnTuto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent ball = new Intent(getApplicationContext(), Balls.class);
+                ball.putExtra(KEY_VALUE_LEVEL, 1);
+                // start the new activity
+                startActivity(ball);
+                overridePendingTransition(R.anim.slide, R.anim.slide_out);
+                finish();
+            }
+        });
+
+        btnGame.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent ball = new Intent(getApplicationContext(), MenuParam.class);
+                startActivity(ball);
+                overridePendingTransition(R.anim.slide, R.anim.slide_out);
+            }
+        });
+
+    }
+
+    public void onBackPressed() {
+        Intent home = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(home);
+        finish();
+    }
+}
