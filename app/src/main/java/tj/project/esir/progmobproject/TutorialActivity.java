144f9eb4001c4c0570d3c992eb6a84ae184ecc42
Yztoc <stephant.thomas.ae@gmail.com>
2018-12-13 01:24:32 +0100

    bug fix | add click img view tutoActivity

--- app/src/main/java/tj/project/esir/progmobproject/TutorialActivity.java
+++ app/src/main/java/tj/project/esir/progmobproject/TutorialActivity.java
@@ -5,9 +5,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-import tj.project.esir.progmobproject.ball_games.Balls;
 import tj.project.esir.progmobproject.ball_games.MenuParam;
 
 public class TutorialActivity extends AppCompatActivity {
@@ -19,6 +20,48 @@ public class TutorialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
+
+        final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.tutoRadioGroup);
+
+
+        ImageView img1 = (ImageView) findViewById(R.id.img_tuto1);
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                game = 1;
+                RadioButton game1 = (RadioButton) findViewById(R.id.game1);
+                game1.setChecked(true);
+                game1.setSelected(true);
+            }
+        });
+
+        ImageView img2 = (ImageView) findViewById(R.id.img_tuto2);
+        img2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                game = 2;
+
+                RadioButton game2 = (RadioButton) findViewById(R.id.game2);
+                game2.setChecked(true);
+                game2.setSelected(true);
+            }
+        });
+
+        ImageView img3 = (ImageView) findViewById(R.id.img_tuto3);
+        img3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                game = 3;
+
+                RadioButton game3 = (RadioButton) findViewById(R.id.game3);
+                game3.setChecked(true);
+                game3.setSelected(true);
+            }
+        });
+
+
+
+
         btnGame = findViewById(R.id.launch_game);
 
         btnGame.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +76,8 @@ public class TutorialActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
 
     }
 
